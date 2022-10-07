09e3b2e39b70f60080396ca30491215e0ad8d663
xed <xed@DESKTOP-K6NONND.localdomain>
2022-10-06 18:01:23 -0700

    working without last param, garbage in the output to fix

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/time.h>
 
 typedef struct s_times {
 	int	eat;
@@ -12,21 +13,20 @@ typedef struct s_times {
 
 typedef struct s_philo {
 	int				id;
+	int				ret;
+	int             state;
+	long			eat_time;
+	t_times			*time;
 	pthread_t       thread;
 	pthread_mutex_t fork;
-	int             state;
-	t_times			*time;
-	pthread_mutex_t	*prev;
 	pthread_mutex_t	*next;
 }	t_philo;
 
 #define USAGE_MSG "Usage:\t./philosopher time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n"
 
-int	error(char *msg, int usage, int ret)
+int	error(char *msg, int ret)
 {
 	printf("Error: %s\n", msg);
-	if (usage)
-		printf(USAGE_MSG);
 	return (ret);
 }
 
@@ -79,6 +79,24 @@ int	ft_atoi(const char *str)
 	return (res);
 }
 
+#define STARVE 4
+#define ERROR -1
+#define SUCCESS 0
+
+long	gettime()
+{
+	static struct timeval	start = {0, 0};
+	struct timeval			now;
+
+	if (start.tv_sec == 0 && start.tv_usec == 0 && \
+	gettimeofday(&start, 0))
+		return (ERROR);
+	if (gettimeofday(&now, 0))
+		return (ERROR);
+	return ((now.tv_sec * 1000000 + now.tv_usec) - \
+	(start.tv_sec * 1000000 + start.tv_usec));
+}
+
 void	msleep(unsigned int ms)
 {
 	ms++;
@@ -103,14 +121,125 @@ bool	parse_argv(char **argv, int *num, t_times *time)
 	return (true);
 }
 
+int	check_starve(int update_eat_time, t_philo *data)
+{
+	long	now;
+
+	now = gettime();
+	if (now == ERROR)
+		return (ERROR);
+	if (data->eat_time && (now / 1000) - data->eat_time > data->time->die)
+		return (true);
+	if (update_eat_time)
+		data->eat_time = now;
+	return (false);
+}
+
+int	print_state(int id, int state)
+{
+	long		now;
+	static char	*msg[5] = {
+		"%ldms: philosopher %d is eating\n",
+		"%ldms: philosopher %d is thinking\n",
+		"%ldms: philosopher %d is sleeping\n",
+		"%ldms: philosopher %d has taken a fork\n"
+		"%ldms: philosopher %d died\n"
+	};
+
+	now = gettime();
+	if (now == ERROR)
+		return (ERROR);
+	printf(msg[state], now / 1000, id);
+	if (state == STARVE)
+		return (STARVE);
+	return (SUCCESS);
+}
+
+int	ph_eat(t_philo *data)
+{
+	int		starve;
+
+	starve = check_starve(true, data);
+	if (starve == true)
+		return (print_state(data->id, STARVE));
+	if (pthread_mutex_lock(&data->fork) || print_state(data->id, 3) || \
+	pthread_mutex_lock(data->next) || print_state(data->id, 3))
+		return (ERROR);
+	if (starve == ERROR || \
+	print_state(data->id, data->state))
+		return (ERROR);
+	msleep(data->time->eat);
+	if (pthread_mutex_unlock(&data->fork) || \
+	pthread_mutex_unlock(data->next))
+		return (ERROR);
+	data->state = 1;
+	return (SUCCESS);
+}
+
+
+int	ph_sleep(t_philo *data)
+{
+	int		starve;
+
+	starve = check_starve(false, data);
+	if (starve == true)
+		return (print_state(data->id, STARVE));
+	if (starve == ERROR || print_state(data->id, data->state))
+		return (ERROR);
+	msleep(data->time->sleep);
+	data->state = 2;
+	return (false);
+}
+
+int	ph_think(t_philo *data)
+{
+	int		starve;
+
+	starve = check_starve(false, data);
+	if (starve == true)
+		return (print_state(data->id, STARVE));
+	if (starve == ERROR || print_state(data->id, data->state))
+		return (ERROR);
+	data->state = 0;
+	return (false);
+}
+
 void	*philosopher_routine(void *arg)
 {
-	t_philo *data = (t_philo *)arg;
+	t_philo *data;
+	int		*ret;
+
+	ret = malloc(sizeof(int));
+	data = (t_philo *)arg;
 	while (1)
 	{
-		
+		if (data->state == 0)
+			*ret = ph_eat(data);
+		else if (data->state == 1)
+			*ret = ph_sleep(data);
+		else if (data->state == 2)
+			*ret = ph_think(data);
+		if (*ret == STARVE)
+			return ((void *) ret);
 	}
+	free(ret);
 	return (0);
+}
+
+void	init_t_philo(t_philo *philosophers, int i, int num, t_times *time)
+{
+	philosophers[i].id = i;
+	philosophers[i].ret = -1;
+	philosophers[i].eat_time = 0;
+	if (i % 2)
+		philosophers[i].state = 2;
+	else
+		philosophers[i].state = 0;
+	if (i != num - 1)
+		philosophers[i].next = &philosophers[i + 1].fork;
+	else
+		philosophers[i].next = &philosophers[0].fork;
+	philosophers[i].time = time;
 }
 
 bool	initialize_philosophers(t_philo	**philosophers_pointer, int num, t_times *time)
@@ -121,37 +250,44 @@ bool	initialize_philosophers(t_philo	**philosophers_pointer, int num, t_times *t
 	i = 0;
 	*philosophers_pointer = malloc(sizeof(t_philo) * num);
 	philosophers = *philosophers_pointer;
+	if (gettime() == ERROR)
+		return (false);
 	while (i != num)
 	{
-		philosophers[i].id = i;
-		philosophers[i].state = 0;
-		if (i)
-			philosophers[i].prev = &philosophers[i - 1].fork;
-		if (i != num - 1)
-		philosophers[i].next = &philosophers[i + 1].fork;
-		philosophers[i].time = time;
-		if (pthread_mutex_init(&philosophers[i].fork, 0) || \
-			pthread_create(&philosophers[i++].thread, 0, \
-			&philosopher_routine, (void *)&philosophers[i]))
+		init_t_philo(philosophers, i, num, time);
+		if (!i && pthread_mutex_init(&philosophers[i].fork, 0))
 			return (false);
+		if (pthread_mutex_init(philosophers[i].next, 0) || \
+		pthread_create(&philosophers[i].thread, 0, \
+		&philosopher_routine, (void *)&philosophers[i]))
+			return (false);
+		i++;
 	}
-	philosophers[num - 1].next = &philosophers[0].fork;
-	philosophers[0].prev = &philosophers[num - 1].fork;
 	return (true);
 }
 
 bool	wait_for_starve(t_philo *philosophers, int num)
 {
 	int	i;
+	int	ret;
 
-	i = 0;
-	while (i != num)
+	ret = -1;
+	while (1)
 	{
-		printf("waiting for %d\n", i);
-		if (pthread_join(philosophers[i].thread, 0))
-			return (false);
-		i++;
+		i = 0;
+		while (i != num)
+		{
+			if (philosophers[i].ret != -1)
+			{
+				ret = philosophers[i].ret;
+				break ;
+			}
+			i++;
+		}
+		if (ret != -1)
+			break ;
 	}
+	printf("finishedddd with %d\n", ret);
 	return (true);
 }
 
@@ -162,13 +298,17 @@ int main(int argc, char **argv)
 	int		ph_num;
 
 	if (argc != 5 && argc != 6)
-		return (error("Wrong number of arguments", 1, 1));
+	{
+		printf(USAGE_MSG);
+		printf("Wrong number of arguments");
+		return (1);
+	}
 	if (!parse_argv(&argv[1], &ph_num, &time))
-		return (error("Error parsing arguments", 0, 2));
+		return (error("Error parsing arguments", 2));
 	if (!initialize_philosophers(&philosophers, ph_num, &time))
-		return (error("Error initializing philosophers", 0, 3));
+		return (error("Error initializing philosophers", 3));
 	if (!wait_for_starve(philosophers, ph_num))
-		return (error("Error waiting for philosophers", 0, 4));
+		return (error("Error waiting for philosophers", 4));
 	free_all(philosophers, ph_num);
 	return (0);
 }
